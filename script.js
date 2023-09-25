3e396338038519b47e1cc0a0e6a637e4e3af91e0
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-09-25 23:30:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 //your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+let MtouristSpots = touristSpots.replace(/^(a | an | the)/i,");
+  console.log(MtouristSpots);										 
