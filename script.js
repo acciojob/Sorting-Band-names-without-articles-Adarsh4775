ad307dafe6dd7d9bc53c921a7bd36c3494fff725
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-12 23:29:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-//your code here
-
-function ArticleRemove(str){
-	str=str.replace(/^(a | an | the)/i,"");
-
-
-	touristSpots.sort((a,b)=>
-		stripArticle(a).localeCompare(stripArticle(b)));
-
-	let ul = document.getElementById("band");
-	touristSpots.forEach(spot =>{
-	let li = document.createElement("li");
-	li.textContent=spot;
-	ul.appendChild(li);
-});
+function ArticleRemove(touristSpots) {
+    function stripArticle(name) {
+        return name.replace(/^(a |an |the )/i, '').trim();
+    }
+    touristSpots.sort((a, b) => stripArticle(a).localeCompare(stripArticle(b)));
+    let ul = document.getElementById("bands");
+    touristSpots.forEach(spot => {
+        let li = document.createElement("li");
+        li.textContent = spot;
+        ul.appendChild(li);
+    });
 }
-	
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+ArticleRemove(touristSpots);
